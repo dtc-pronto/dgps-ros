ca98a20df4002973505b31aafa4b0bfe0eedf915
Joshua Caswell <joshuaacaswell@gmail.com>
2026-06-08 12:21:18 -0400

    bag script for testing

--- run-dev.bash
+++ run-dev.bash
@@ -1,11 +1,17 @@
 #!/bin/bash
 
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+BAG_DIR="${SCRIPT_DIR}/bags"
+
+mkdir -p "${BAG_DIR}"
+
 xhost +
 docker run -it --rm \
     --network=host \
     --ipc=host \
     --privileged \
     -v "/dev:/dev" \
+    -v "${BAG_DIR}:/home/dtc/bags" \
     -v "/tmp/.X11-unix:/tmp/.X11-unix" \
     -e DISPLAY=$DISPLAY \
     -e QT_X11_NO_MITSHM=1 \
